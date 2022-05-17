6dff7eaf6873ed57215ae9361d7f7df810600ab8
zheng-rui-20080218 <517247354@qq.com>
2022-05-18 00:10:14 +0800

    Changes to be committed: 	new file:   get_angle/__pycache__/get_information.cpython-39.pyc 	new file:   get_angle/calculate.py 	new file:   get_angle/get_information.py 	deleted:    get_angle/get_position.py

---
+++ get_angle/calculate.py
@@ -0,0 +1,56 @@
+import math as m
+import datetime as dt
+import pysolar as psl
+import get_information
+
+def sun_position(latitude,longitude,time_zone):
+    """
+    This is a module can calculate sun's position
+    Please input two floats - latitude and longitude and one int - time_zone
+    Output is a dictionary - {altitude_angle,azimuth_angle}
+    The unit of measurement is degree(°) and hour(h)
+    It uses a package - pysolar to give the answer precisely
+    This is pysolar's website https://github.com/pingswept/pysolar/
+    You can also use "pip install pysolar" to download it
+    By Duan-JunHan
+    """
+    local_tz = dt.timezone(dt.timedelta(hours=time_zone))
+    date = dt.datetime.now(tz=local_tz)
+    print(date)
+    altitude_angle = psl.solar.get_altitude(latitude, longitude, date)
+    azimuth_angle = psl.solar.get_azimuth(latitude, longitude, date)
+    return_dic = {'altitude_angle':altitude_angle,'azimuth_angle':azimuth_angle}
+    return return_dic
+
+def main():
+    print("Auto following System Settings: ")
+    while True:
+        flag = input("Do you want to use the default setting[Y/N] ->")
+        if flag == "Y" or flag == "y":
+            latitude = get_information.get_position()["latitude"]
+            longitude = get_information.get_position()["longitude"]
+            time_zone = get_information.get_timezone()
+            angle = sun_position(latitude,longitude,time_zone)
+            print("altitude angle: ",angle["altitude_angle"] )
+            print("azimuth angle: ",angle["azimuth_angle"] )
+            break
+        elif flag == "N" or flag == "n":
+            while True:
+                latitude = input("Please input your latitude ->")
+                longitude = input("Please input your longitude ->")
+                time_zone = input("Please input your timezone ->")
+                try:
+                    angle = sun_position(latitude,longitude,time_zone)
+                    break
+                except:
+                    print("Someting went wrong! Please try again!")
+
+            print("altitude angle: ",angle["altitude_angle"] )
+            print("azimuth angle: ",angle["azimuth_angle"] )
+            break
+        else:
+            print("We don't have this choice! Please enter again!")
+        print("Setting successfully!")
+
+if __name__ == "__main__":
+    main()
---
+++ get_angle/get_information.py
@@ -0,0 +1,26 @@
+import os 
+import time
+import geoip2.database
+
+def get_position():
+    os.system("curl ifconfig.me > IP.info")
+
+    with open("IP.info", "r", encoding="utf-8") as f:
+        ip = f.read()
+
+    print("IP:", ip)
+
+    reader = geoip2.database.Reader("get_angle\geoip2_database\GeoLite2-City.mmdb")
+    data = reader.city(ip)
+    latitude = data.location.latitude
+    longitude = data.location.longitude
+    print("Latitude:", latitude)
+    print("Longitude:", longitude)
+    return {"latitude":latitude, "longitude":longitude}
+
+def get_timezone():
+    now = time.time()
+    now = time.localtime(now)
+    timezone = time.strftime("%z", now)
+    timezone = int(timezone[:3])
+    return timezone
