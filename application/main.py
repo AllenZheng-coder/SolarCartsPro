07cc0ca9af8162cca62db60f154e29262b1ce995
zheng-rui-20080218 <517247354@qq.com>
2022-05-24 13:02:50 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch Zheng-rui
    Your branch is up to date with 'origin/Zheng-rui'.
    
    Changes to be committed:
    	new file:   application/__pycache__/database.cpython-39.pyc
    	modified:   application/__pycache__/function.cpython-39.pyc
    	modified:   application/main.py
    	modified:   application/static/indexm.css
    	new file:   application/static/login.css
    	modified:   application/templates/indexm.html
    	modified:   application/templates/login.html
    	renamed:    application/templates/test.html -> application/templates/temperature.html

--- application/main.py
+++ application/main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from errno import ESTALE
+from flask import Flask, redirect
 from flask import render_template
 from flask import request
 from flask import session
 import function
 import datetime
+import database
 import os
-
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -19,7 +21,7 @@ def main():
     session["mobile"] = is_mobile
 
     if session["mobile"]:
-        return render_template("indexm.html")
+        return render_template("indexm.html", date=datetime.datetime.now().strftime(r"%Y年%m月%d日  %H:%M:%S"))
     else:
         return render_template("indexm.html")
 
@@ -29,6 +31,19 @@ def shows(content):
         return render_template("{}.html".format(content))
     except:
         return "404"
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        username = request.values.get("username")
+        password = request.values.get("password")
+        print(username, password)
+        if database.login(username) == -1:
+            return render_template("login.html", reminder="用户名或密码错误！")
+        else:
+            return redirect("/")
+    else:
+        return render_template("login.html",  reminder="欢迎登录")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8080")
